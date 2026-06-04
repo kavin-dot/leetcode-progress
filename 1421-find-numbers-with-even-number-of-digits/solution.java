c10075325cfa9d426eae4627464f5cdd746a2f20
kavin s <kavinsp06@gmail.com>
2026-06-04 12:29:56 +0000

    Sync LeetCode submission Runtime - 1 ms (99.49%), Memory - 44.5 MB (78.16%)

---
+++ 1421-find-numbers-with-even-number-of-digits/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            int digitcount=0;
+            while(num>0){
+
+                num=num/10;
+                digitcount++;
+            }
+            if(digitcount%2==0){
+                count++;
+            }
+        }
+            return count;
+    }
+}
+            
