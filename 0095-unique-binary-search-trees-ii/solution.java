9303b308d7ad6575f8b6609569623f6e30c64e88
kavin s <kavinsp06@gmail.com>
2026-08-19 09:32:10 +0000

    Sync LeetCode submission Runtime - 1 ms (99.36%), Memory - 46 MB (92.18%)

---
+++ 0095-unique-binary-search-trees-ii/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+
+        return build(1, n);
+    }
+
+    private List<TreeNode> build(int start, int end) {
+
+        List<TreeNode> result = new ArrayList<>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+        for (int root = start; root <= end; root++) {
+
+            List<TreeNode> leftTrees = build(start, root - 1);
+            List<TreeNode> rightTrees = build(root + 1, end);
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+
+                    TreeNode node = new TreeNode(root);
+
+                    node.left = left;
+                    node.right = right;
+
+                    result.add(node);
+                }
+            }
+        }
+
+        return result;
+    }
+}
