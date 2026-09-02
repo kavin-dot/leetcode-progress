b0e24c6e5ca900869264638a696b50769ceafcff
kavin s <kavinsp06@gmail.com>
2026-09-02 04:56:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (84.08%)

---
+++ 0086-partition-list/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode large = new ListNode(0);
+
+        ListNode s = small;
+        ListNode l = large;
+
+        while (head != null) {
+            if (head.val < x) {
+                s.next = head;
+                s = s.next;
+            } else {
+                l.next = head;
+                l = l.next;
+            }
+
+            head = head.next;
+        }
+
+        l.next = null;
+        s.next = large.next;
+
+        return small.next;
+    }
+}
